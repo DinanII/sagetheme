ac1a86abcff5a37dd3ccd126aae42e06a16df618
Dinan <dinanvanw@gmail.com>
2025-05-03 14:04:56 +0200

    Added demo blade template

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,8 +1,7 @@
-<header class="banner">
-  <a class="brand" href="{{ home_url('/') }}">
+<header class="banner flex justify-center m-2 bg-blue-400 p-2 rounded-2xl">
+  <a class="brand text-white font-bold no-underline" href="{{ home_url('/') }}">
     {!! $siteName !!}
   </a>
-
   @if (has_nav_menu('primary_navigation'))
     <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
       {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
---
+++ resources/views/template-blade-demo.blade.php
@@ -0,0 +1,10 @@
+{{-- Blade Demo Template --}}
+@extends('layouts.app')
+
+@section('content')
+@while ('have_posts()') @php(the_post())
+  <div class="template full-width-template no-header">
+  @includeFirst(['partials.content-page','partials.content'])
+  </div>
+@endwhile
+@endsection
